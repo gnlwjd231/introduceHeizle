07504b25a56bdaf55dde62dd18fb63648a71f119
Hwi Jeong Park <heee2x1@gmail.com>
2023-04-11 18:29:54 +0900

    firstcommit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './styles/main.scss';
+
+function Main() {
+  return (
+    <main>
+        <h1>heizle introduce</h1>
+        <h2>프론트엔드 개발자 박휘정을 소개합니다.</h2>
+    </main>
+  );
+}
+
+export default Main;
